ce43007592325ff5a8f974c69ab3d28efc8e2645
Shailendrayadav18 <{shailendrakyadav1872002@gmail.com}>
2026-06-03 17:20:43 +0530

    Make recent search component better

--- GitHub-Repo-Explorer/client/src/components/search/RecentSearches.jsx
+++ GitHub-Repo-Explorer/client/src/components/search/RecentSearches.jsx
@@ -6,24 +6,32 @@ function RecentSearches({
     return null;
 
   return (
-    <div className="flex flex-wrap gap-2 mt-4">
-      {searches.map((item) => (
-        <button
-          key={item}
-          onClick={() =>
-            onSelect(item)
-          }
-          className="
-          px-3
-          py-1
-          bg-gray-100
-          rounded-full
-          hover:bg-gray-200
-          "
-        >
-          {item}
-        </button>
-      ))}
+    <div className="mt-4">
+      <p className="text-sm text-gray-500 mb-2">
+        Recent Searches
+      </p>
+
+      <div className="flex flex-wrap gap-2">
+        {searches.map((item) => (
+          <button
+            key={item}
+            onClick={() =>
+              onSelect(item)
+            }
+            className="
+            px-3
+            py-1
+            bg-white
+            border
+            rounded-full
+            shadow-sm
+            hover:bg-blue-50
+            "
+          >
+            {item}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
