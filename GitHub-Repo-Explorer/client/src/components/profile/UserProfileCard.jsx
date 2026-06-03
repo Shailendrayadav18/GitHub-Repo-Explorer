9f625778ecdb957db69d3a386572e3da1cbab50f
Shailendrayadav18 <{shailendrakyadav1872002@gmail.com}>
2026-06-03 17:19:39 +0530

    Added link of user github profile feature

--- GitHub-Repo-Explorer/client/src/components/profile/UserProfileCard.jsx
+++ GitHub-Repo-Explorer/client/src/components/profile/UserProfileCard.jsx
@@ -1,6 +1,7 @@
 import {
   Users,
   BookOpen,
+  ExternalLink,
 } from "lucide-react";
 
 function UserProfileCard({
@@ -13,13 +14,11 @@ function UserProfileCard({
       rounded-2xl
       shadow-lg
       p-6
-      sticky
-      top-4
       "
     >
       <img
         src={user.avatar_url}
-        alt={user.name}
+        alt={user.login}
         className="
         w-28
         h-28
@@ -29,35 +28,33 @@ function UserProfileCard({
       />
 
       <h2 className="text-center text-2xl font-bold mt-4">
-        {user.name}
+        {user.name ||
+          user.login}
       </h2>
 
       <p className="text-center text-gray-500">
         @{user.login}
       </p>
 
-      <p className="mt-4 text-center">
-        {user.bio}
-      </p>
+      {user.bio && (
+        <p className="mt-4 text-center">
+          {user.bio}
+        </p>
+      )}
 
       <div className="grid grid-cols-3 gap-4 mt-6 text-center">
-
         <div>
           <Users />
-          <p>
-            {user.followers}
-          </p>
-          <span>
+          <p>{user.followers}</p>
+          <span className="text-sm">
             Followers
           </span>
         </div>
 
         <div>
           <Users />
-          <p>
-            {user.following}
-          </p>
-          <span>
+          <p>{user.following}</p>
+          <span className="text-sm">
             Following
           </span>
         </div>
@@ -67,12 +64,31 @@ function UserProfileCard({
           <p>
             {user.public_repos}
           </p>
-          <span>
+          <span className="text-sm">
             Repos
           </span>
         </div>
-
       </div>
+
+      <a
+        href={user.html_url}
+        target="_blank"
+        rel="noreferrer"
+        className="
+        mt-6
+        flex
+        items-center
+        justify-center
+        gap-2
+        bg-black
+        text-white
+        py-3
+        rounded-xl
+        "
+      >
+        View GitHub Profile
+        <ExternalLink size={16} />
+      </a>
     </div>
   );
 }
